292c52bd96540bc934c91e3f180f74e06f70a6d7
AditiKhandelia <khandeliaaditi@gmail.com>
2024-10-10 15:15:39 +0530

    Add Main program

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <queue>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <cmath>
+#include <ctime>
+#include <cstdlib>
+#include <cstdio>
+#include <cstring>
+#include <utility>
+#include <unordered_map>
+
+extern void printDelimiter();
+
+#include "interface.cpp"
+
+using namespace std;
+
+int main()
+{
+    MainInterface mainInterface;
+    mainInterface.runInterface();
+}
